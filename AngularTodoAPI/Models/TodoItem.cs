508c2e0b7032831759cbfaa93508a261b20250d8
JorgeAlvarado <JorgeAlvarado@DEV-PC-1>
2026-02-23 12:16:13 -0500

    Add project files.

---
+++ AngularTodoAPI/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngularTodoAPI.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
